0afabb03111d13927bdaf25fbfe1dc24ecfe5886
Cristopher Fonseca <cristopher.fonseca@kmm.com.br>
2022-11-14 09:15:28 -0300

    Testing CI

--- src/test/java/com/study/integration/controllers/PersonControllerTest.java
+++ src/test/java/com/study/integration/controllers/PersonControllerTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -29,6 +31,7 @@ import com.study.integration.entities.Person;
 import com.study.integration.services.PersonService;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 @TestPropertySource(locations={"classpath:application.properties"})
 public class PersonControllerTest {
