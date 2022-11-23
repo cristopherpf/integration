3033f8036f3f7ba63097979a3998d03b3b762b4a
Cristopher Fonseca <cristopher.fonseca@kmm.com.br>
2022-11-23 19:38:21 -0300

    Changing from utils to service

---
+++ src/main/java/com/study/integration/services/EmailService.java
@@ -0,0 +1,6 @@
+package com.study.integration.services;
+
+public interface EmailService {
+
+	boolean isValid(String emailAddress);
+}
---
+++ src/main/java/com/study/integration/services/impl/EmailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.study.integration.services.impl;
+
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.study.integration.services.EmailService;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
+	
+	private final static String REGEX_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" 
+	        + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+	
+	public boolean isValid(String emailAddress) {
+		logger.info("E-mail validado: {}", emailAddress);
+	    if(emailAddress == null)
+	    	return false;
+	    
+		return Pattern.compile(REGEX_PATTERN)
+	                  .matcher(emailAddress)
+	                  .matches();
+	}
+}
